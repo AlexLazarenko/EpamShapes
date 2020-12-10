825e486b92222635fd0dad6bd2d26b403b1bb848
AlexLazarenko <lazarenko.aa89@gmail.com>
2020-12-10 11:22:06 +0300

    fix

--- src/main/java/edu/epam/shape/parser/TrianglePointParser.java
+++ src/main/java/edu/epam/shape/parser/TrianglePointParser.java
@@ -2,6 +2,8 @@ package edu.epam.shape.parser;
 
 import edu.epam.shape.entity.Point2d;
 import edu.epam.shape.entity.Triangle;
+import edu.epam.shape.exception.ParserException;
+import edu.epam.shape.exception.ValidatorException;
 import edu.epam.shape.validator.TriangleValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,16 +14,18 @@ import java.util.List;
 public class TrianglePointParser {
     private static final Logger logger = LogManager.getLogger(TrianglePointParser.class);
 
-    public List<Triangle> trianglesFromFile(List<String> lines){ //throws ParserException {
+    public List<Triangle> trianglesFromFile(List<String> lines) throws ValidatorException, ParserException { //throws ParserException {
         List<Triangle> triangleList = new ArrayList<>();
         List<Point2d> point2DList= new ArrayList<>();
         List<Double> coordinatesList=new ArrayList<>();
         for (String line : lines) {
             String[] numbers = line.split("\\s+");
-            if (numbers.length!=6){
-                logger.warn("To create triangle three points needed!");
-             //   throw new ParserException("To create triangle three points needed!");//todo
-                continue;
+            try {
+                if (numbers.length!=6){
+                      throw new ParserException("Wrong data!To create triangle three points needed!");//todo
+                }
+            }catch (ParserException e){
+                logger.warn(e);
             }
             for (String number : numbers) {
                 if (number.matches("\\d+$")) {
--- src/main/java/edu/epam/shape/validator/TriangleValidator.java
+++ src/main/java/edu/epam/shape/validator/TriangleValidator.java
@@ -1,6 +1,7 @@
 package edu.epam.shape.validator;
 
 import edu.epam.shape.entity.Triangle;
+import edu.epam.shape.exception.ValidatorException;
 import edu.epam.shape.utility.ShapeUtility;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +11,7 @@ import java.util.List;
 public class TriangleValidator {
     private static final Logger logger = LogManager.getLogger(TriangleValidator.class);
 
-    public List<Triangle> validateList(List<Triangle> triangleList) {
+    public List<Triangle> validateList(List<Triangle> triangleList) throws ValidatorException {
         for (Triangle triangle : triangleList) {
             if (!isTriangle(triangle)) {
                 triangleList.remove(triangle);
@@ -19,17 +20,20 @@ public class TriangleValidator {
         return triangleList;
     }
 
-    public boolean isTriangle(Triangle triangle) {//throws ValidatorException {
+    public boolean isTriangle(Triangle triangle) throws ValidatorException {
         boolean isTriangle = true;
         double aTob = ShapeUtility.calculateDistance(triangle.getA(), triangle.getB());
         double bToc = ShapeUtility.calculateDistance(triangle.getB(), triangle.getC());
         double aToc = ShapeUtility.calculateDistance(triangle.getA(), triangle.getC());
         if (aTob > bToc + aToc || bToc > aTob + aToc || aToc > aTob + bToc) {// test input data
-            isTriangle = false;
-            logger.warn("Triangle {} with sides {},{},{} can not exist! Wrong data!", triangle, aTob, bToc, aToc);
-            // throw new ValidatorException("Triangle not exist wrong data");//todo
+            try{
+                throw new ValidatorException("Triangle not exist wrong data");//todo
+            } catch (ValidatorException e){
+                isTriangle = false;
+                logger.warn("Triangle {} with sides {},{},{} can not exist! Wrong data!", triangle, aTob, bToc, aToc);
+            }
         }
-        logger.info("validation suc");
+        logger.info("validation successful");
         return isTriangle;
     }
 }
